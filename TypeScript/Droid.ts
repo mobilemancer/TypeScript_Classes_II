41ec93074290b2748b057ccbae87b7833eebc6ea
mobilemancer <andrkarl@gmail.com>
2015-11-20 17:06:09 +0100

    Obi-Wan

--- TypeScript/Droid.ts
+++ TypeScript/Droid.ts
@@ -7,21 +7,21 @@ class Droid {
 	}
 
 	get holographicMessage(): string {
-		if (this.receiver === "Obi Wan Kenobi") {
+		if (this.receiver === "Obi-Wan Kenobi") {
 			return this.holoMessage;
 		}
 	}
 
 	public enterPassCode(code: number): void {
 		if (code === 1234) {
-			this.receiver = "Obi Wan Kenobi";
+			this.receiver = "Obi-Wan Kenobi";
 		}
 	}
 }
 
-var droidTest1 = (function() {
+(function() {
 	var droid = new Droid();
-	droid.holographicMessage = "Help me Obi Wan Kenobi, you are my only hope!";
+	droid.holographicMessage = "Help me Obi-Wan Kenobi, you are my only hope!";
 
 	console.log(droid.holographicMessage);
 
