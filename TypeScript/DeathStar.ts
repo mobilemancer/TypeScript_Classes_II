3ae0f6c4807ca3a52b8406902ce64698a57d5a2e
mobilemancer <andrkarl@gmail.com>
2015-11-21 02:01:48 +0100

    DeathStar!

---
+++ TypeScript/DeathStar.ts
@@ -0,0 +1,13 @@
+class DeathStar {
+	static fireSuperLaser(): string {
+		return "Wait, we're powering up...";
+	}
+
+	fireStuff(): string {
+		return "Wait, we're powering up...";
+	}
+}
+
+(function() {
+	console.log(DeathStar.fireSuperLaser());
+})();
--- TypeScript/Droid.ts
+++ TypeScript/Droid.ts
@@ -2,11 +2,15 @@ class Droid {
 	private holoMessage: string;
 	private receiver: string;
 
-	set holographicMessage(message: string) {
+	public set holographicMessage(message: string) {
 		this.holoMessage = message;
 	}
 
-	get holographicMessage(): string {
+	public get holographicMessage(): string {
+		if (!this.receiver) {
+			return "Invalid user!";
+		}
+
 		if (this.receiver === "Obi-Wan Kenobi") {
 			return this.holoMessage;
 		}
@@ -23,11 +27,11 @@ class Droid {
 	var droid = new Droid();
 	droid.holographicMessage = "Help me Obi-Wan Kenobi, you are my only hope!";
 
-	console.log(droid.holographicMessage);
+	console.log(droid.holographicMessage);	// "Invalid user!"
 
 	droid.enterPassCode(42);
-	console.log(droid.holographicMessage);
+	console.log(droid.holographicMessage);	// "Invalid user!"
 
 	droid.enterPassCode(1234);
-	console.log(droid.holographicMessage);
+	console.log(droid.holographicMessage);	// "Help me Obi Wan Kenobi, you are my only hope!"
 })();
